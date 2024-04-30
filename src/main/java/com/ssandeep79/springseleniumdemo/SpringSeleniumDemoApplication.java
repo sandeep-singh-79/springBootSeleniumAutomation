bb3cb6c273d16422da8acb1dfdc8886f89f15d5e
Sandeep Singh <s.sandeep79@ymail.com>
2024-04-30 06:46:11 +0000

    Adding first set of changes

---
+++ src/main/java/com/ssandeep79/springseleniumdemo/SpringSeleniumDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ssandeep79.springseleniumdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeleniumDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSeleniumDemoApplication.class, args);
+	}
+
+}
