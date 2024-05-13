fe4dda7206239b577b64452f48c76680df475363
Sandeep Singh <s.sandeep79@ymail.com>
2024-05-13 17:03:32 +0530

    Download Resources

--- src/test/java/com/ssandeep79/springseleniumdemo/resource/ResourceTest.java
+++ src/test/java/com/ssandeep79/springseleniumdemo/resource/ResourceTest.java
@@ -1,13 +1,19 @@
 package com.ssandeep79.springseleniumdemo.resource;
 
 import com.ssandeep79.springseleniumdemo.SpringBaseTestNGTest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
 import org.testng.annotations.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 
+@Slf4j
 public class ResourceTest extends SpringBaseTestNGTest {
     @Value("classpath:data/testdata.csv")
     private Resource classPathResource;
@@ -15,19 +21,31 @@ public class ResourceTest extends SpringBaseTestNGTest {
     private Resource fileResource;
     @Value("https://www.google.com")
     private Resource urlResource;
+    @Value("https://www.w3.org/TR/PNG/iso_8859-1.txt")
+    private Resource downloadResource;
+
+    @Value("${screenshot.path}/some.txt")
+    private Path downloadPath;
 
     @Test
     public void testResource() throws IOException {
-        System.out.println("------------------" + classPathResource.getURL() + "-----------------");
-        System.out.println(classPathResource.getFilename());
+        log.info("------------------ {} -----------------", classPathResource.getURL());
+        log.info(classPathResource.getFilename());
         Files.readAllLines(classPathResource.getFile().toPath()).forEach(System.out::println);
 
-        System.out.println("------------------" + fileResource.getURL() + "-----------------");
-        System.out.println(fileResource.getFilename());
+        log.info("------------------ {} -----------------", fileResource.getURL());
+        log.info(fileResource.getFilename());
         Files.readAllLines(fileResource.getFile().toPath()).forEach(System.out::println);
 
-        System.out.println("------------------" + urlResource.getURL() + "-----------------");
-        System.out.println(urlResource.getFilename());
-        System.out.println(urlResource.getInputStream().readAllBytes());
+        log.info("------------------ {} -----------------", urlResource.getURL());
+        log.info(urlResource.getFilename());
+        System.out.println(Arrays.toString(urlResource.getInputStream().readAllBytes()));
+    }
+
+    @Test (expectedExceptions = FileNotFoundException.class)
+    public void testDownloadResource() throws IOException {
+        log.info("------------------ {} -----------------", downloadResource.getURL());
+        log.info(downloadResource.getFilename());
+        FileCopyUtils.copy(downloadResource.getInputStream(), Files.newOutputStream(downloadPath));
     }
 }
