a8fe4b430b8324a8c926104e6783dfcd354eb8b1
Sandeep Singh <s.sandeep79@ymail.com>
2024-07-01 14:50:17 +0530

    automation bot check bypass code put in place for chromedriver

--- src/main/java/com/ssandeep79/springseleniumdemo/demo/config/WebDriverConfig.java
+++ src/main/java/com/ssandeep79/springseleniumdemo/demo/config/WebDriverConfig.java
@@ -5,6 +5,7 @@ import com.ssandeep79.springseleniumdemo.demo.annotation.ThreadScopeBean;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Profile;
@@ -26,6 +27,12 @@ public class WebDriverConfig {
     @ConditionalOnProperty(name = "browser", havingValue = "chrome", matchIfMissing = true)
     public WebDriver chromeDriver() {
         WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        options.setExperimentalOption("useAutomationExtension", false);
+        var chromeDriver = new ChromeDriver(options);
+        chromeDriver.executeScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})");
+        return chromeDriver;
     }
 }
